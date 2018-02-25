d8f315ea529b0550965f1f327488a541d719a275
harshithharsha <36638928+harshithharsha@users.noreply.github.com>
2018-02-25 09:34:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
+$(date)
+</div>
+<div>
+$(content)
+</div>
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
