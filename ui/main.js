1da9fa9ba6ee68c37187821e84d502fe055eaccd
harshithharsha <36638928+harshithharsha@users.noreply.github.com>
2018-02-25 09:34:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+var button=document.getelement("counter");
+
+var counter = 0;
+button.oneclick=function(){
+    
+    
+    
+    counter =counter +1;
+    var spam = document.getElementbyID("count");
+    spam.innerHTML = counter.tostring();
+}
